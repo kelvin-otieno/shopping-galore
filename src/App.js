2d864655411e755cfea190d9d3231612a5607f74
kelvin-otieno <kelvinotieno06@gmail.com>
2020-06-19 13:20:07 +0300

    added nested routes for the collection page and removed all the individual collection pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import "./App.css";
 import HomePage from "./components/pages/homepage/homepage";
 import Shop from "./components/pages/shop/shop";
 import Header from "./components/header/header";
-import SignInAndSignUpPage from "./components/pages/signinandsignuppage/SignInAndSignUpPage";
+import SignInAndSignUpPage from "./components/pages/sign-in-and-sign-up/sign-in-and-sign-up-page";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import setCurrentUser from "./redux/user/user.action";
 import { connect } from "react-redux";
-import Hats from "./components/pages/hats/hats";
-import Jackets from "./components/pages/jackets/jackets";
-import Sneakers from "./components/pages/sneakers/sneakers";
-import Womens from "./components/pages/womens/womens";
-import Mens from "./components/pages/mens/mens";
+
+
+// import Hats from "./components/pages/hats/hats";
+// import Jackets from "./components/pages/jackets/jackets";
+// import Sneakers from "./components/pages/sneakers/sneakers";
+// import Womens from "./components/pages/womens/womens";
+// import Mens from "./components/pages/mens/mens";
 import CheckoutPage from "./components/pages/checkout/checkout-page";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "./redux/user/user.selectors";
@@ -56,7 +58,7 @@ class App extends Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={Shop} />
+          <Route path="/shop" component={Shop} />
           <Route
             exact
             path="/signin"
@@ -68,11 +70,11 @@ class App extends Component {
               )
             }
           />
-          <Route exact path="/shop/hats" component={Hats} />
+          {/* <Route exact path="/shop/hats" component={Hats} />
           <Route exact path="/shop/jackets" component={Jackets} />
           <Route exact path="/shop/sneakers" component={Sneakers} />
           <Route exact path="/shop/womens" component={Womens} />
-          <Route exact path="/shop/mens" component={Mens} />
+          <Route exact path="/shop/mens" component={Mens} /> */}
           <Route exact path="/checkout" component={CheckoutPage} />
         </Switch>
       </div>
